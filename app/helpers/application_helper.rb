7fad2e34a9a7eadff081657c017f84ed5307cca0
Berkay Akbas <berkayakbas@gmail.com>
2022-11-22 14:58:13 +0300

    Add styles for splash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_bar_content(controller)
+    if controller == 'categories'
+      'Categories'
+    elsif controller == 'splash'
+      'Welcome'
+    else
+      'Not implemented'
+    end
+  end
 end
