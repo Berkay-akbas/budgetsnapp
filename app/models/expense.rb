af328dd72beeb81d716470938aec14a3fec422de
Berkay Akbas <berkayakbas@gmail.com>
2022-11-24 17:34:40 +0300

    Sort back button

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id', optional: true
   belongs_to :category, foreign_key: 'category_id', optional: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
